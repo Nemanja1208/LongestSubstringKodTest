038263beedb6bad86bbd39efad94c3f26a446316
Ward <wardbeniamin@hotmail.se>
2025-03-10 12:58:59 +0100

    Ahmed har kommenterat

--- Program.cs
+++ Program.cs
@@ -17,39 +17,42 @@ namespace LongestSubstringKodTest
 
             foreach (var s in testStrings)
             {
+                // int för resultat fint
                 int result = LongestUniqueSubstringLength(s);
+
+                //console för visa de display så bra jobbat
                 Console.WriteLine($"Input: \"{s}\" -> Longest unique substring length: {result}");
             }
-
+            //console för visa de display så bra jobbat text för att avsluta
             Console.WriteLine("\nPress any key to exit...");
             Console.ReadKey();
         }
 
         public static int LongestUniqueSubstringLength(string s)
         {
-            int maxLength = 0; // Håller koll på längsta delsträngen
-            int count = 0; // Räknar tecken i nuvarande delsträng
+            int maxLength = 0; // Bra att du Håller koll på längsta delsträngen
+            int count = 0; // Detta är bra här räknar man nuvarande delsträng
 
-            // Loopar igenom varje tecken i strängen
+            // du har lagt till Loopar  för att igenom för varje tecken i strängen
             for (int i = 0; i < s.Length; i++)
             {
-                count = 0; // Nollställ räkningen för varje startposition
+                count = 0; // bra att  du Nollställ räkningen för varje startposition
 
-                // Kollar resten av strängen från position `i`
+                // ser bra ut att du Kollar resten av strängen från position `i`
                 for (int j = i; j < s.Length; j++)
                 {
                     // Om tecknet redan finns i den undersökta delsträngen, avbryt
                     if (s.Substring(i, count).Contains(s[j]))
                         break;
 
-                    count++; // Öka längden på unika delsträngen
+                    count++; // duktig mario som checkar att Öka längden på unika delsträngen
                 }
 
                 // Uppdatera max längden om vi hittat en längre unika sekvens
                 if (count > maxLength)
                     maxLength = count;
             }
-
+            // här retunerara vi max lengd
             return maxLength;
         }
     }
