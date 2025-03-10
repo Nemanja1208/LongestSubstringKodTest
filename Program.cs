dcf3de5d09ae3e1539531f2d6320d6563bfb183d
TomasSteifo <Steifotomas@gmail.com>
2025-03-10 10:51:39 +0100

    //Ultimate new feature

--- Program.cs
+++ Program.cs
@@ -40,10 +40,49 @@
         //-----------------------------------------------------
         public static int LongestUniqueSubstringLength(string s)
         {
-            // Currently returns -1 so you know it's not implemented yet
-            // Replace this logic with your solution
-            return -1;
+            // This dictionary will map each character to the last index at which it appeared.
+            Dictionary<char, int> lastSeenIndices = new Dictionary<char, int>();
+
+            // We'll keep track of the maximum length of any substring
+            // we've encountered that doesn't contain repeated characters.
+            int maxLength = 0;
+
+            // 'startIndex' represents the left boundary of our current substring (the sliding window).
+            int startIndex = 0;
+
+            // We iterate through the string using 'i' as the right boundary of the sliding window.
+            for (int i = 0; i < s.Length; i++)
+            {
+                char currentChar = s[i];
+
+                // If the current character has been seen before AND
+                // its last seen position is within the current sliding window
+                // (i.e., >= startIndex), we need to move 'startIndex' forward
+                // so we don't include the repeated character in our substring.
+                if (lastSeenIndices.ContainsKey(currentChar) && lastSeenIndices[currentChar] >= startIndex)
+                {
+                    // Move 'startIndex' to one position after the previous occurrence of 'currentChar'.
+                    // This effectively "skips over" the old repeated character, ensuring our new window is distinct.
+                    startIndex = lastSeenIndices[currentChar] + 1;
+                }
+
+                // Update or add the current character's last seen position to the current index 'i'.
+                lastSeenIndices[currentChar] = i;
+
+                // The size of our current non-repeating substring is (i - startIndex + 1).
+                int currentWindowSize = i - startIndex + 1;
+
+                // If this current window is larger than any we've seen before, update 'maxLength'.
+                if (currentWindowSize > maxLength)
+                {
+                    maxLength = currentWindowSize;
+                }
+            }
+
+            // After processing all characters, 'maxLength' holds the length of the longest
+            // substring with unique (non-repeating) characters.
+            return maxLength;
         }
-        //-----------------------------------------------------
+
     }
 }
